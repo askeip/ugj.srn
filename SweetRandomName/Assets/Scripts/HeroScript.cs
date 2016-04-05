9f9af545d5dbd4a9ce67b29405a3cf89b7223d66
askeip <paparazzi730@gmail.com>
2016-04-05 15:59:10 +0500

    Hero added, simple movement

---
+++ SweetRandomName/Assets/Scripts/HeroScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroScript : MonoBehaviour 
+{
+	public float xSpeed;
+	public float ySpeed;
+
+	Rigidbody2D playerRigidbody2D;
+	bool facingRight;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+
+	void Start()
+	{
+		playerRigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		if (grounded && Input.GetKeyDown (KeyCode.Space)) 
+		{
+			playerRigidbody2D.velocity = new Vector2(playerRigidbody2D.velocity.x, ySpeed);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+
+		float xMove = Input.GetAxis ("Horizontal");
+
+
+		playerRigidbody2D.velocity = new Vector2 (xSpeed * xMove,playerRigidbody2D.velocity.y);
+
+		if (xMove > 0 && !facingRight)
+			Flip ();
+		else if (xMove < 0 && facingRight)
+			Flip ();
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
