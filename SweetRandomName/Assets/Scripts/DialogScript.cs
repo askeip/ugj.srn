56c13fb952ba11a1d107eddb986951fa0052bf14
askeip <paparazzi730@gmail.com>
2016-04-14 16:02:47 +0500

    First lvl v0.8.1

--- SweetRandomName/Assets/Scripts/DialogScript.cs
+++ SweetRandomName/Assets/Scripts/DialogScript.cs
@@ -4,20 +4,30 @@ using System.Collections;
 public class DialogScript : WorldObject
 {
     public string dialog;
-    public float time;
+    public float textShowingTime;
+    public bool shouldOff;
+    private bool active = true;
 
     void Start()
     {
+        active = true;
         GeneralStart();
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && active)
         {
             var playerScript = other.gameObject.GetComponent<HeroScript>();
             playerScript.text.text = dialog;
-            playerScript.ShowText(time);
+            playerScript.ShowText(textShowingTime);
+            active = !shouldOff;
         }
+    }
+
+    public override void Reset()
+    {
+        active = true;
+        base.Reset();
     }
 }
---
+++ SweetRandomName/Assets/Scripts/InstructionsTabScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionsTabScript : WorldObject 
+{
+    public GameObject player;
+    public GameObject panel;
+    private HeroScript heroScript;
+
+	void Start () {
+        GeneralStart();
+        heroScript = player.GetComponent<HeroScript>();
+	}
+	
+	void Update () {
+        panel.SetActive(heroScript.HasGlasses);
+	}
+}
