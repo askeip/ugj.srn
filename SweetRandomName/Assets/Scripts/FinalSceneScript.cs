022104078338f06dade2a1360a5988a309f27a11
askeip <paparazzi730@gmail.com>
2016-04-17 02:12:54 +0500

    Prefinal version

---
+++ SweetRandomName/Assets/Scripts/FinalSceneScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FinalSceneScript : MonoBehaviour 
+{
+    public GameObject Girl;
+    public GameObject Kiss;
+    public Image panelImage;
+
+    private float startTime;
+    public float EndingLatency;
+    private float endingTimeStart;
+
+    void Start()
+    {
+        endingTimeStart = EndingLatency;
+    }
+
+    public void OnTriggerEnter2D(Component other)
+    {
+        var rot = Girl.transform.rotation;
+        rot.y = 0f;
+        Girl.transform.rotation = rot;
+        Kiss.SetActive(true);
+        startTime = Time.time;
+        EndGame();
+    }
+
+    void EndGame()
+    {
+        StartCoroutine(End());
+        Invoke("ExitToMainMenu", EndingLatency);
+    }
+
+    IEnumerator End()
+    {
+        while(startTime + EndingLatency > Time.time)
+        {
+            var clr = panelImage.color;
+            clr.a = (Time.time - startTime) / EndingLatency;
+            panelImage.color = clr;
+            yield return null;
+        }
+            
+    }
+    void ExitToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
